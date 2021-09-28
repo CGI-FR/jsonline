e3f2448e490bba063861ef6f5748e6d17186df22
Adrien Aury <adrienaury@gmail.com>
2021-09-28 10:14:02 +0200

    docs: license headers update

--- pkg/jsonline/errors.go
+++ pkg/jsonline/errors.go
@@ -1,19 +1,19 @@
 // Copyright (C) 2021 CGI France
 //
-// This file is part of JL.
+// This file is part of the jsonline library.
 //
-// JL is free software: you can redistribute it and/or modify
+// The jsonline library is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
 //
-// JL is distributed in the hope that it will be useful,
+// The jsonline library is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 // GNU General Public License for more details.
 //
 // You should have received a copy of the GNU General Public License
-// along with JL.  If not, see <http://www.gnu.org/licenses/>.
+// along with the jsonline library.  If not, see <http://www.gnu.org/licenses/>.
 //
 // Linking this library statically or dynamically with other modules is
 // making a combined work based on this library.  Thus, the terms and
